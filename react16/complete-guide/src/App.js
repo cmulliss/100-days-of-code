6b9521c892fd553aba20df3cefb6927e56bae5f1
Cherry Mulliss <cmulliss@gmail.com>
2018-03-10 19:12:20 +0000

    passing method reference and data binding

--- react16/complete-guide/src/App.js
+++ react16/complete-guide/src/App.js
@@ -12,14 +12,48 @@ export default class App extends Component {
   };
   // can access state in render method using 'this'
   // which refers to the class
-  switchNameHandler = () => {
-    console.log("was clicked");
+  // can pass a referene to this handler as property
+  // to our component, click={}
+  switchNameHandler = newName => {
+    //console.log("was clicked");
+    this.setState({
+      persons: [
+        { name: newName, age: 17 },
+        { name: "Pengy", age: 9 },
+        { name: "Feathers", age: 11 }
+      ]
+    });
   };
+  // use default event object
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: "Motley", age: 17 },
+        { name: event.target.value, age: 9 },
+        { name: "Feathers", age: 11 }
+      ]
+    });
+  };
+  /*how pass newName? Can use bind to handle 'this'
+  and use a list or arguments passed into our function
+  as one way of passing an argument.
+  Also, can use => and pass arguments directly. 
+  Is an anonymous function, that doesn't get called 
+  until clicked, so can use ()
+  May be better to use bind, depending on size of app
+  as can be inefficient without.
+  */
   render() {
     return (
       <div className="App">
         <h1>I am a react app</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button onClick={this.switchNameHandler.bind(this, "Penguin")}>
+          Switch Name
+        </button>
+        <p />
+        <button onClick={() => this.switchNameHandler("Mot!!")}>
+          Switch Name 2
+        </button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -27,6 +61,8 @@ export default class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+          click={this.switchNameHandler}
+          changed={this.nameChangedHandler}
         />
         <Person
           name={this.state.persons[2].name}
--- react16/complete-guide/src/person/person.js
+++ react16/complete-guide/src/person/person.js
@@ -1,13 +1,14 @@
 import React from "react";
 
-const Person = props => {
-  const randomAge = Math.floor(Math.random() * 30);
+const Person = (props) => {
+  // const randomAge = Math.floor(Math.random() * 30);
   return (
     <div>
-      <p>{props.children}</p>
-      <p>
-        I am a {props.name}, I am {props.age} years old
+      <p onClick={props.click}>
+        I am {props.name}, I am {props.age} years old
       </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} />
     </div>
   );
 };
