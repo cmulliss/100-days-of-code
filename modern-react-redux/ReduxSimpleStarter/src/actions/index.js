087531473ebaefef071bfe3d8a45bacfafebb62b
Cherry Mulliss <cmulliss@gmail.com>
2018-03-26 14:38:09 +0100

    more Redux

--- modern-react-redux/ReduxSimpleStarter/src/actions/index.js
+++ modern-react-redux/ReduxSimpleStarter/src/actions/index.js
@@ -3,6 +3,12 @@
 // needs to run through all our redcuers, so
 // need to connect it to Redux
 
+//selectBook is an ActionCreator, in needs to return
+// an object with a type property
 export function selectBook(book) {
-    console.log('A book has been selected', book.title)
+  //console.log('A book has been selected', book.title)
+  return {
+    type: "BOOK_SELECTED",
+    payload: book
+  };
 }
--- modern-react-redux/ReduxSimpleStarter/src/reducers/index.js
+++ modern-react-redux/ReduxSimpleStarter/src/reducers/index.js
@@ -1,10 +1,12 @@
-import { combineReducers } from 'redux';
-import BooksReducer from './reducer_books';
+import { combineReducers } from "redux";
+import BooksReducer from "./reducer_books";
+import ActiveBook from "./reducer_active_book";
 
 //this is really th emapping of our state
 //key, books and the value is BooksReducer
 const rootReducer = combineReducers({
- books: BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
